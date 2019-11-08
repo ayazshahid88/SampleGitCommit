638b6bcadf140f839863ca59d1377c9629e26dae
Ayaz Shahid <ayaz.shahid@yahoo.com>
2019-11-07 20:44:32 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
